6bc976b88f5abc4e050c2cf3cdb21553298dee3e
inasekin <nasekinid8591@yandex.ru>
2025-02-28 21:04:44 +0300

    fix: поправил билд

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,3 @@
-import { Checkbox } from '@/components/ui/checkbox'
-import { Button } from '@/components/ui/button'
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-
 export const MainPage = () => {
     return (
         <div className="min-h-screen">
