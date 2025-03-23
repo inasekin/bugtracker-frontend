0613785d0e30cc479ee481a5880367aa2bc9b0ab
Мызников Алексей <myznikov@mcad.ru>
2025-03-23 19:55:40 +0600

    fix: добавлен диалог добавления пользователь-роли в проекты

--- src/components/project/data/project-dto.tsx
+++ src/components/project/data/project-dto.tsx
@@ -1,11 +1,30 @@
+export interface Dictionary<T> {
+    [Key: string]: T;
+};
+
+export interface UserRoleDictionary {
+    [Key: string]: string;
+};
+
+export type IssueCategoryDto = {
+    categoryName: string;
+    userId? : string;
+};
+
 export type ProjectDto = {
     id: string,
     name: string,
-    description: string
-}
+    description: string,
+    userRoles: UserRoleDictionary
+    versions: string[]
+    issueCategories:  IssueCategoryDto[]
+};
 
 export const defaultProjectDto: ProjectDto = {
         id: "",
         name: "",
-        description: ""
+        description: "",
+        versions: [],
+        issueCategories: [],
+        userRoles: {}
 };
--- src/components/project/project-dialog.tsx
+++ src/components/project/project-dialog.tsx
@@ -9,7 +9,7 @@ import {ProjectContext} from "./data/project-context"
 import {useNavigate} from "react-router-dom";
 import {useFetchState} from "./data/use-fetch";
 import {useState} from "react"
-import {putData, postData, deleteData} from "./data/fetch-data";
+import {putData, postData} from "./data/fetch-data";
 
 import {
     Card,
@@ -21,95 +21,64 @@ import {
 } from "@/components/ui/card"
 
 export type ProjectDialogProps = {
-    projectId: string;
-    isNewProject?: boolean;
-    isDeleteProject?: boolean;
+    projectId?: string,
+    isNewProject?: boolean
 };
 
-export const ProjectDialog = ({projectId, isNewProject, isDeleteProject} : ProjectDialogProps) => {
+export const ProjectDialog = ({projectId, isNewProject} : ProjectDialogProps) => {
 
     const [data, setData] = isNewProject ? useState(defaultProjectDto) :  useFetchState<ProjectDto>('/api/project/' + projectId, defaultProjectDto)
     const navigate = useNavigate();
 
-    if(isDeleteProject)
-    {
-       const navigate = useNavigate();
-        
-        const deleteCmd = async () => {
-            await deleteData('/api/project/' + projectId);
-            navigate('/projects', { state: {} });
-        };
-    
-        return (
-            <Card className="w-fit">
+    const save = async () => {
+        if(isNewProject)
+        {
+            await postData('/api/project/', data);
+        }
+        else {
+            await putData('/api/project/' + projectId, data);
+        }
+        navigate('/projects', { state: {} });
+    };
+
+    return (
+        <ProjectContext.Provider value={{ project: data, setProject: setData }}>
+            <Card>
             <CardHeader>
-            <CardTitle>Удаление проекта</CardTitle>
-            <CardDescription>
-                Вы уверены, что хотите удалить проект?
-            </CardDescription>
+            <CardTitle>Редактирование проекта {data.name}</CardTitle>
+            <CardDescription>Проект - основа для управления задачами. Содержит задачи. 
+                На проект назначаются пользователи с определенными ролями. 
+                Для проекта можно задать список версий и список доступных категорий(модулей/подпроектов)</CardDescription>
             </CardHeader>
             <CardContent>
-            {data.name}
+
+                <Tabs defaultValue="project" className="flex flex-col items-start">
+                    <TabsList>
+                        <TabsTrigger value="project">Свойства</TabsTrigger>
+                        <TabsTrigger value="users">Пользователи</TabsTrigger>
+                        <TabsTrigger value="versions">Версии</TabsTrigger>
+                        <TabsTrigger value="categories">Категории</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="project" className="w-full">
+                        <ProjectCard />
+                    </TabsContent>
+                    <TabsContent value="users" className="w-full">
+                        <UsersList />
+                    </TabsContent>
+                    <TabsContent value="versions" className="w-full">
+                        <VersionsList />
+                    </TabsContent>
+                    <TabsContent value="categories" className="w-full">
+                        <CategoriesList />
+                    </TabsContent>
+                </Tabs>
+
             </CardContent>
-                <CardFooter className="flex justify-center  gap-4">
-                <Button id="deleteCmd" onClick={ () => deleteCmd() }>Да</Button>
-                <Button id="back" onClick={ ()=>  navigate('/projects') }>Нет</Button>
+                <CardFooter className="flex items-start gap-4">
+                <Button id="saveButton" onClick={ () => save() }>Сохранить</Button>
+                <Button id="back" onClick={ ()=>  navigate('/projects') }>Отмена</Button>
                 </CardFooter>
             </Card>
-        )    
-    }
-    else 
-    {
-        const save = async () => {
-            if(isNewProject)
-            {
-                await postData('/api/project/', data);
-            }
-            else {
-                await putData('/api/project/' + projectId, data);
-            }
-            navigate('/projects', { state: {} });
-        };
-    
-        return (
-            <ProjectContext.Provider value={{ project: data, setProject: setData }}>
-                <Card>
-                <CardHeader>
-                <CardTitle>Редактирование проекта {data.name}</CardTitle>
-                <CardDescription>Проект - основа для управления задачами. Содержит задачи. 
-                    На проект назначаются пользователи с определенными ролями. 
-                    Для проекта можно задать список версий и список доступных категорий(модулей/подпроектов)</CardDescription>
-                </CardHeader>
-                <CardContent>
-
-                    <Tabs defaultValue="project" className="flex flex-col items-start">
-                        <TabsList>
-                            <TabsTrigger value="project">Свойства</TabsTrigger>
-                            <TabsTrigger value="users">Пользователи</TabsTrigger>
-                            <TabsTrigger value="versions">Версии</TabsTrigger>
-                            <TabsTrigger value="categories">Категории</TabsTrigger>
-                        </TabsList>
-                        <TabsContent value="project" className="w-full">
-                            <ProjectCard />
-                        </TabsContent>
-                        <TabsContent value="users" className="w-full">
-                            <UsersList />
-                        </TabsContent>
-                        <TabsContent value="versions" className="w-full">
-                            <VersionsList />
-                        </TabsContent>
-                        <TabsContent value="categories" className="w-full">
-                            <CategoriesList />
-                        </TabsContent>
-                    </Tabs>
-
-                </CardContent>
-                    <CardFooter className="flex items-start gap-4">
-                    <Button id="saveButton" onClick={ () => save() }>Сохранить</Button>
-                    <Button id="back" onClick={ ()=>  navigate('/projects') }>Отмена</Button>
-                    </CardFooter>
-                </Card>
-            </ProjectContext.Provider>
-        )
-    }
+        </ProjectContext.Provider>
+    )
 }
